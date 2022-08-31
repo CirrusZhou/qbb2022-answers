5a9c8c25bbd32ec7e61d1f1e4e30d888eefc7175
Ailin <azhou22@jhu.edu>
2022-08-30 22:40:46 -0400

    fully commented script for exercise2

--- day2-lunch/analysis.py
+++ day2-lunch/analysis.py
@@ -6,20 +6,30 @@ from bed_parser import parse_bed
 # specify the file name
 fname = "hg38_gencodev41_chr21.bed"
 
-# run the function
+# run the function and store the returned result in bed
 bed = parse_bed(fname)
 
+# create a list 
 exon_num = []
 
+# for everyline in bed
 for i in bed:
+    # get the exon number and append it into the list.
     exon_num.append(i[9])
-    
+
+# sort the list to find the median    
 exon_num.sort()
+# let's see how many numbers are in the list
 total = len(exon_num)
+
+# if the amount number is odd, just pick the middlemost number to be median
 if type(total/2) ==float:
     answer = exon_num[int(total/2 + 0.5)]
+# if the amount number is even, median is the average of the two middlemost numbers
 else:
-    answer = exon_num[int(total/2)]
+    answer = (exon_num[int(total/2)] + exon_num[int(total/2)+1])/2
+
+# give the reader some feedback
 print(f"There are {total} numbers in the exon_num list.")
 print(f"The median should be the {total/2} th number in the list.")
 print(f"Which should be {answer}")
